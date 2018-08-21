f2f77a3fb520b5b545ef53a3e5094b385e11336f
Aaron Weissman <weissman.aaron@gmail.com>
2018-08-21 13:16:13 -0600

    make new game button show canvas. Start hidden

--- test/Game-test.js
+++ test/Game-test.js
@@ -12,12 +12,15 @@ const ctx = {
 
 
 describe('Game', () => {
+  let game;
+  beforeEach(() => {
+    game = new Game(ctx);
+  })
+
   it('should end the game if blocks collides with wall', () => {
-    const game = new Game(ctx);
   })
 
   it('should have properties', () => {
-    const game = new Game(ctx);
 
     assert.equal(game.lives, 5);
     assert.equal(game.level, 1);
@@ -27,14 +30,12 @@ describe('Game', () => {
   })
 
   it('should end game', () => {
-    const game = new Game(ctx);
 
     game.endGame();
     assert.equal(game.gameOver, true);
   })
 
   it('should lose a life on collision with a car', () => {
-    const game = new Game(ctx);
 
     game.yellowCars[0].x = 100;
     game.yellowCars[0].y = 100;
@@ -45,7 +46,6 @@ describe('Game', () => {
   })
 
   it('should lose a life if it lands on water', () => {
-    const game = new Game(ctx);
 
     game.frog[0].x = 140;
     game.frog[0].y = 230;
@@ -56,7 +56,6 @@ describe('Game', () => {
   })
 
   it('should earn 100 points if frog lands in right spot at the top', () => {
-    const game = new Game(ctx);
 
     game.frog[0].x = 49;
     game.frog[0].y = 7;
